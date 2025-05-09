ab6bed93c2b69323508f4cc52a73e55a6cc74b8b
ichynul <ichynul@163.com>
2025-05-09 13:08:38 +0800

    更多配置

--- src/config/process.php
+++ src/config/process.php
@@ -5,6 +5,8 @@ $user = config('server.user', '');
 $group = config('server.group', '');
 $listen = config('server.listen', 'http://0.0.0.0:8787');
 $port = explode(':', $listen)[2] ?? 8787;
+$event_loop = config('server.event_loop', '');
+$max_package_size = config('server.max_package_size');
 $process = [];
 
 for ($i = 1; $i < $process_count; $i += 1) {
@@ -14,6 +16,8 @@ for ($i = 1; $i < $process_count; $i += 1) {
         'count' => 1, // 进程数
         'user' => $user,
         'group' => $group,
+        'event_loop' => $event_loop,
+        'max_package_size' => $max_package_size,
         'constructor' => [
             'request_class' => \support\Request::class, // request类设置
             'logger' => \support\Log::channel('default'), // 日志实例
